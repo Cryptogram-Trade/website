5e7284e2f660ed8e1a66df1127ac0a9fd41458b0
David Lopez <d.lpz@yandex.com>
2021-04-27 20:19:37 +0200

    added monero article

--- app/blog/views.py
+++ app/blog/views.py
@@ -6,7 +6,7 @@ class Article:
     self.title = title
     self.body = body
     self.slug = slug
-    self.image = image
+    self.image = "https://mlnjwsydcfjh.i.optimole.com/lv2uaCs-tc4l_59X/w:300/h:200/q:auto/" + image
 
 
 class ListView(TemplateView):
@@ -17,10 +17,16 @@ class ListView(TemplateView):
         ctx["articles"] = [
           Article(
             title="Shrimpy with Binance for US citizens",
-            image="https://mlnjwsydcfjh.i.optimole.com/lv2uaCs-tc4l_59X/w:300/h:200/q:auto/https://news.trijo.co/wp-content/uploads/2019/09/binance-i-usa-oppnar-for-registrering-nasta-vecka.jpg",
+            image="https://news.trijo.co/wp-content/uploads/2019/09/binance-i-usa-oppnar-for-registrering-nasta-vecka.jpg",
             body="Binance clients based in the US have a special Binance platform that is hosted on Binance.us. If you're a US citizen, you must register on Binance.us. Binance.com is reserved for non US citizens.",
             slug="shrimpy-for-binance-us-users"
-          )
+          ),
+          Article(
+            title="Monero is about to have a momentum in 2021",
+            image="https://insidebitcoins.com/wp-content/uploads/2019/03/Monero-Price-Analysis-1.jpg",
+            body="Binance clients based in the US have a special Binance platform that is hosted on Binance.us. If you're a US citizen, you must register on Binance.us. Binance.com is reserved for non US citizens.",
+            slug="monero-momentum-in-2021"
+          ),
         ]
         return ctx
 
